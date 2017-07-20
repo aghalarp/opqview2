2e4bebccee4d3ba2f2758d088a3b42a8896db4ef
David Aghalarpour <aghalarp@hawaii.edu>
2017-07-20 12:56:38 -1000

    Add default client side import for flashMessage component.

--- app/imports/startup/client/index.js
+++ app/imports/startup/client/index.js
@@ -4,6 +4,9 @@ import './routes';
 import '../../ui/utils/globalTemplateHelpers.js';
 import '../../ui/stylesheets/site.css';
 
+// Component Templates
+import '../../ui/components/flashMessage/flashMessage.js';
+
 // Get rid of the default __blaze-root div element and use the regular <body> element instead.
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 BlazeLayout.setRoot('body');
