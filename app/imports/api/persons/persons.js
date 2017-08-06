801a15d85dc6536c456bda04f61006a21f5e754e
David Aghalarpour <aghalarp@hawaii.edu>
2017-08-05 16:19:04 -1000

    Refactor to implement BaseCollection class.

--- app/imports/api/persons/persons.js
+++ app/imports/api/persons/persons.js
@@ -1,22 +1,42 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
+import BaseCollection from '../base/BaseCollection.js';
 
-Persons = new Mongo.Collection("persons", {idGeneration: "MONGO"});
+class PersonCollection extends BaseCollection {
+  constructor() {
+    super('Person',
+        null,
+        new SimpleSchema({
+          userId: { // Reference to Users. (From Meteor's Accounts-Password package.)
+            type: String,
+            regEx: SimpleSchema.RegEx.Id // Meteor's string ID, not Mongo's ObjectID.
+          },
+          firstName: {
+            type: String
+          },
+          lastName: {
+            type: String
+          }
+        }),
+        null
+    );
 
-// Create and attach schema for this collection
-PersonsSchema = new SimpleSchema({
-  userId: { // Reference to Users. (From Meteor's Accounts-Password package.)
-    type: String,
-    regEx: SimpleSchema.RegEx.Id // Meteor's string ID, not Mongo's ObjectID.
-  },
-  firstName: {
-    type: String
-  },
-  lastName: {
-    type: String
+    this.publicationNames = {
+
+    }
   }
-});
 
-Persons.attachSchema(PersonsSchema);
+  define({userId, firstName, lastName}) {
+    const personId = this._collection.insert({userId, firstName, lastName});
+    return personId;
+  }
 
-export default Persons;
+  publish() {
+
+  }
+
+}
+
+/**
+ * Provides the singleton instance of this class.
+ * @type {PersonCollection}
+ */
+export const Persons = new PersonCollection();
